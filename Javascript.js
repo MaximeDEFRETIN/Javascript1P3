2374599bf42d03e862d4cd8f4bdcc7a3d2afd89d
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-10-27 15:18:28 +0200

    Correction

--- Javascript.js
+++ Javascript.js
@@ -1,12 +1,8 @@
 function imgover() {
   document.getElementById('img').src="img/image1_2.jpg";
-  document.getElementById('img').style.borderColor='red';
-  document.getElementById('img').style.borderWidth='3px';
-  document.getElementById('img').style.borderStyle='solid';
+  document.getElementById('img').style.border='red solid 3px';
 }
 function imgout() {
   document.getElementById('img').src="img/image1.jpg";
-  document.getElementById('img').style.borderColor='none';
-  document.getElementById('img').style.borderWidth='none';
-  document.getElementById('img').style.borderStyle='none';
+  document.getElementById('img').style.border='none';
 }
